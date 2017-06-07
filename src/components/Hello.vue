e042608618394e4d3e8146f273d928e11fd32eb9
杜萍萍 <dupingping@eefung.com>
2017-06-07 15:21:37 +0800

    first commit

---
+++ src/components/Hello.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h1 class="apple">hello apple!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less" rel="stylesheet/less">
+@import "./home.less";
+</style>
+
